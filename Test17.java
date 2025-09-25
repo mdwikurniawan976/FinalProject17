20ce04a4cb6b0dff10adb91db3357cda78dadd08
mdwikurniawan976 <xyvnn478@gmail.com>
2025-09-25 08:18:44 +0700

    mengubah baris

--- Test17.java
+++ Test17.java
@@ -1,6 +1,6 @@
 public class Test17 {
     public static void main(String[] args) {
         System.out.println("Hi, ini baris kode pertama");
-        System.out.println("User 2 menambahkan baris baru");
+        System.out.println("User 1 menambahkan baris baru");
     }
 }
